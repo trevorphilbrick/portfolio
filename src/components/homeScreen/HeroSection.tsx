751665b91c090f490c7c86802630eac71ba7b3bb
Trevor <trevorphilbrick@outlook.com>
2023-04-10 21:04:21 -0700

    added mousover watcher

--- src/components/homeScreen/HeroSection.tsx
+++ src/components/homeScreen/HeroSection.tsx
@@ -5,11 +5,11 @@ const HeroSection = () => {
   const [width, setWidth] = useState<number>(0)
   const ref = useRef<any>(null)
 
-  const renderRow = () => {
+  const renderRow = (iterative: number) => {
     const rowArray = []
     for (let i = 0 ; i < width/40 - 1 ; i++) {
       rowArray.push( 
-      <div className="dot-container">
+      <div className="dot-container" id={`row${iterative}Pos${i}`}>
       <div className="dot"></div>
       </div>)
     }
@@ -17,10 +17,13 @@ const HeroSection = () => {
     return rowArray
   }
 
+  document.onmouseover = function(e: any) {
+    console.log(e.target.id);
+  }
   const renderGrid = () => {
     const columnArray = []
     for (let i = 0 ; i < height/40 - 1; i++) {
-      columnArray.push(<div style={{display: "flex"}}>{renderRow()}</div>)
+      columnArray.push(<div style={{display: "flex"}}>{renderRow(i)}</div>)
     }
     console.log(columnArray.length)
     return columnArray
