93f658ec32ccafb5876f382d9786308bddcf04bc
Trevor <trevorphilbrick@outlook.com>
2023-04-10 19:52:37 -0700

    got smart grid for dots working

--- src/components/homeScreen/HeroSection.tsx
+++ src/components/homeScreen/HeroSection.tsx
@@ -1,7 +1,40 @@
+import{ useEffect, useRef, useState } from "react";
+
 const HeroSection = () => {
+  const [height, setHeight] = useState<number>(0)
+  const [width, setWidth] = useState<number>(0)
+  const ref = useRef<any>(null)
+
+  const renderRow = () => {
+    const rowArray = []
+    for (let i = 0 ; i < width/40 - 1 ; i++) {
+      rowArray.push( 
+      <div className="dot-container">
+      <div className="dot"></div>
+      </div>)
+    }
+    console.log(rowArray.length)
+    return rowArray
+  }
+
+  const renderGrid = () => {
+    const columnArray = []
+    for (let i = 0 ; i < height/40 - 1; i++) {
+      columnArray.push(<div style={{display: "flex"}}>{renderRow()}</div>)
+    }
+    console.log(columnArray.length)
+    return columnArray
+  }
+
+
+  useEffect(() => {
+    setHeight(ref?.current?.clientHeight)
+    setWidth(ref?.current?.clientWidth)
+    console.log(height, width)
+  })
   return (
-    <div className="hero-section">
-      <div className="dot">.</div>
+    <div className="hero-section" ref={ref}>
+      {renderGrid()}
     </div>
   );
 };
