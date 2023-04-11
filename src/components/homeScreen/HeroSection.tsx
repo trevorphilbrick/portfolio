e2de5988f87c95b77776a9f2dfda8375bdeb4a50
Trevor <txp2308@yum.com>
2023-04-11 11:15:28 -0700

    removed complicated layout

--- src/components/homeScreen/HeroSection.tsx
+++ src/components/homeScreen/HeroSection.tsx
@@ -1,29 +1,7 @@
 import HeroSectionBackground from "./HeroSectionBackground";
 
 const HeroSection = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <HeroSectionBackground />
-      <img
-        id="hero-image"
-        src={require("../../assets/heroImage.png")}
-        alt="hero"
-        style={{
-          zIndex: 100,
-          position: "relative",
-          width: "300px",
-          height: "300px",
-        }}
-      />
-    </div>
-  );
+  return <HeroSectionBackground />;
 };
 
 export default HeroSection;
