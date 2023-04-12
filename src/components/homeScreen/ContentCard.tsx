793ae51e9d0a13533e3e048fb5c46b5e015e3815
Trevor <txp2308@yum.com>
2023-04-11 22:15:58 -0700

    Created basic content card and passed data to it

--- src/components/homeScreen/ContentCard.tsx
+++ src/components/homeScreen/ContentCard.tsx
@@ -1,10 +1,33 @@
+type ContentProps = {
+  image: string;
+  title: string;
+  description: string;
+  link: string;
+};
 
 type ContentCardProps = {
-    content: {};
-    onPress?: () => void;
-}
+  content: ContentProps;
+  onPress?: () => void;
+};
 
-const ContentCard = ({ content, onPress }: Props) => {
-}
+const ContentCard = ({ content, onPress }: ContentCardProps) => {
+  return (
+    <div className="content-card" onClick={onPress}>
+      <div className="content-card-image-wrapper">
+        <img
+          className="content-card-image"
+          src={content.image}
+          alt={content.title}
+        />
+      </div>
+      <div className="content-card-content">
+        <h3 className="content-card-content-title">{content.title}</h3>
+        <p className="content-card-content-description">
+          {content.description}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default ContentCard;
--- src/components/homeScreen/ContentSection.tsx
+++ src/components/homeScreen/ContentSection.tsx
@@ -1,45 +1,49 @@
-import { Container, Row, Col } from 'react-grid-system'
-import '../../css/ContentSection.scss'
-import ContentCard from './ContentCard';
+import { Container, Row, Col } from "react-grid-system";
+import "../../css/ContentSection.scss";
+import ContentCard from "./ContentCard";
+import { useNavigate } from "react-router-dom";
 
 const CONTENT = [
   {
-    title: 'Explore',
-    description: 'Explore my projects and learn more about me.',
-    image: require('../../assets/heroImage.png'),
-    link: '/projects',
+    title: "Explore",
+    description: "Explore my projects and learn more about me.",
+    image: require("../../assets/heroImage.png"),
+    link: "/projects",
   },
   {
-    title: 'Blog',
-    description: 'Read my blog posts and learn more about my thoughts.',
-    image: require('../../assets/heroImage.png'),
-    link: '/blog',
+    title: "Blog",
+    description: "Read my blog posts and learn more about my thoughts.",
+    image: require("../../assets/heroImage.png"),
+    link: "/blog",
   },
   {
-    title: 'Photography',
-    description: 'View my photography and learn more about my interests.',
-    image: require('../../assets/heroImage.png'),
-    link: '/photography',
-  }
-]
-
+    title: "Photography",
+    description: "View my photography and learn more about my interests.",
+    image: require("../../assets/heroImage.png"),
+    link: "/photography",
+  },
+];
 
 const ContentSection = () => {
+  const navigate = useNavigate();
   return (
     <div
       className="content-section"
       style={{ marginTop: "100vh", backgroundColor: "#202020" }}
     >
-      <Container >
+      <Container>
         <Row>
           <Col>
-            <h2 className='content-header'>Explore</h2>
+            <h2 className="content-header">Explore</h2>
           </Col>
         </Row>
         <Row>
           {CONTENT.map((content, index) => (
             <Col key={index} xs={12} sm={12} md={4} lg={4}>
-              <ContentCard content={content} />
+              <ContentCard
+                content={content}
+                onPress={() => navigate(content.link)}
+              />
             </Col>
           ))}
         </Row>
