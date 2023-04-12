02ffcf11bdf25f93bcabfdd79a21f10985952027
Trevor <trevorphilbrick@outlook.com>
2023-04-11 19:23:36 -0700

    added contentcard component

---
+++ src/components/homeScreen/ContentCard.tsx
@@ -0,0 +1,10 @@
+
+type ContentCardProps = {
+    content: {};
+    onPress?: () => void;
+}
+
+const ContentCard = ({ content, onPress }: Props) => {
+}
+
+export default ContentCard;
--- src/components/homeScreen/ContentSection.tsx
+++ src/components/homeScreen/ContentSection.tsx
@@ -1,19 +1,49 @@
+import { Container, Row, Col } from 'react-grid-system'
+import '../../css/ContentSection.scss'
+import ContentCard from './ContentCard';
+
+const CONTENT = [
+  {
+    title: 'Explore',
+    description: 'Explore my projects and learn more about me.',
+    image: require('../../assets/heroImage.png'),
+    link: '/projects',
+  },
+  {
+    title: 'Blog',
+    description: 'Read my blog posts and learn more about my thoughts.',
+    image: require('../../assets/heroImage.png'),
+    link: '/blog',
+  },
+  {
+    title: 'Photography',
+    description: 'View my photography and learn more about my interests.',
+    image: require('../../assets/heroImage.png'),
+    link: '/photography',
+  }
+]
+
+
 const ContentSection = () => {
   return (
     <div
       className="content-section"
       style={{ marginTop: "100vh", backgroundColor: "#202020" }}
     >
-      <div className="content-section__title">
-        <h2>Content Section</h2>
-      </div>
-      <div className="content-section__content">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          voluptates, quod quia, quae, quibusdam quidem quos voluptatem
-          voluptatum quas quia.
-        </p>
-      </div>
+      <Container >
+        <Row>
+          <Col>
+            <h2 className='content-header'>Explore</h2>
+          </Col>
+        </Row>
+        <Row>
+          {CONTENT.map((content, index) => (
+            <Col key={index} xs={12} sm={12} md={4} lg={4}>
+              <ContentCard content={content} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
