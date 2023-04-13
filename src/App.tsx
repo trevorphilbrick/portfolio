837db4f95c278ad11c8de01c35005baebc364b12
Trevor <trevorphilbrick@outlook.com>
2023-04-13 11:27:47 -0700

    initialized firebase

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import PhotographyPage from "./routes/PhotographyPage";
 import Navbar from "./components/Navbar";
 import BlogPage from "./routes/BlogPage";
 import ProjectsPage from "./routes/ProjectsPage";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +29,22 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUviuU68Gp7txMcVhjP4BrhHb2fR4CayA",
+  authDomain: "portfolio-8b661.firebaseapp.com",
+  projectId: "portfolio-8b661",
+  storageBucket: "portfolio-8b661.appspot.com",
+  messagingSenderId: "194119104372",
+  appId: "1:194119104372:web:ba606ce3f9ceeb0c0e9e21",
+  measurementId: "G-H1VEXE0DS6"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log(analytics)
   return (
     <div>
       <Navbar />
